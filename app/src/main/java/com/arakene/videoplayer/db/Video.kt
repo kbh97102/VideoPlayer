3821513b603696bbfb8611dd05b5b2083e491684
arakene <arakene_dev@naver.com>
2024-12-16 20:40:22 +0900

    feat: uri 같은 경우 덮어씌움

--- app/src/main/java/com/arakene/videoplayer/db/Video.kt
+++ app/src/main/java/com/arakene/videoplayer/db/Video.kt
@@ -3,11 +3,12 @@ package com.arakene.videoplayer.db
 import android.graphics.Bitmap
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = [Index(value = ["uri"], unique = true)])
 data class Video(
-    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
     @ColumnInfo(name = "uri") val uri: String,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "thumbnail") val thumbnail : Bitmap
--- app/src/main/java/com/arakene/videoplayer/db/VideoDao.kt
+++ app/src/main/java/com/arakene/videoplayer/db/VideoDao.kt
@@ -3,11 +3,12 @@ package com.arakene.videoplayer.db
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface VideoDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun install(video: Video)
 
     @Delete
