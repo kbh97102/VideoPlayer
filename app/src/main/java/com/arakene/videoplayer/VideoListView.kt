41de6e2bfdcb7dc17ddb825aaaa60c3847dd95cf
arakene <arakene_dev@naver.com>
2024-12-16 20:14:43 +0900

    feat: 비디오 다중 선택 contract로 변경

--- app/src/main/java/com/arakene/videoplayer/VideoListView.kt
+++ app/src/main/java/com/arakene/videoplayer/VideoListView.kt
@@ -1,37 +1,32 @@
 package com.arakene.videoplayer
 
-import android.content.Context
-import android.net.Uri
-import android.os.Build
-import androidx.compose.runtime.Composable
-import android.Manifest
 import android.content.ContentResolver
-import android.content.ContentUris
 import android.database.Cursor
 import android.graphics.Bitmap
+import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
 import android.util.Size
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import java.util.concurrent.TimeUnit
 
 
 @Composable
 fun VideoListView(
 
-){
+) {
     val context = LocalContext.current
 
     var bitMap: Bitmap? by remember {
@@ -40,28 +35,31 @@ fun VideoListView(
 
 // Activity Result Launcher for picking a document
     val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.OpenDocument()
-    ) { uri: Uri? ->
-        uri?.let {
-            // Callback when a file is selected
-//            onFileSelected(it)
-            Log.d(">>>>", "uri $it")
+        contract = ActivityResultContracts.PickMultipleVisualMedia()
+    ) { list ->
+        list.map { uri ->
+            uri.let {
+                // Callback when a file is selected
+        //            onFileSelected(it)
+                Log.d(">>>>", "uri $it")
 
-            getThumbnailFromContentUri(context.contentResolver, it)?.let { result ->
-                bitMap = result
-            }
+                getThumbnailFromContentUri(context.contentResolver, it)?.let { result ->
+                    bitMap = result
+                }
 
-            getVideoMetadata(context.contentResolver, it).also { map ->
-                Log.d(">>>>", "Map ${map.toString()}")
+                getVideoMetadata(context.contentResolver, it).also { map ->
+                    Log.d(">>>>", "Map $map")
+                }
             }
         }
+
     }
 
 
 
     Column {
         Button(onClick = {
-            launcher.launch(arrayOf("video/*")) // Filter for video files
+            launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly)) // Filter for video files
         }) {
             Text(text = "Select Video")
         }
@@ -110,10 +108,13 @@ fun getVideoMetadata(contentResolver: ContentResolver, contentUri: Uri): Map<Str
     cursor?.use {
         if (it.moveToFirst()) {
             metadata["title"] = it.getString(it.getColumnIndexOrThrow(MediaStore.Video.Media.TITLE))
-            metadata["duration"] = it.getLong(it.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION))
+            metadata["duration"] =
+                it.getLong(it.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION))
             metadata["size"] = it.getLong(it.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE))
-            metadata["dateAdded"] = it.getLong(it.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED))
-            metadata["mimeType"] = it.getString(it.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE))
+            metadata["dateAdded"] =
+                it.getLong(it.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED))
+            metadata["mimeType"] =
+                it.getString(it.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE))
         }
     }
 
